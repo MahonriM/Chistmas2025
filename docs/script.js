edbe04405175a80ee7e91b5f32d58f5b72f3da62
MahonriM <cripto3000z@gmail.com>
2025-12-24 10:39:15 -0600

    Update script.js
    Adding else if AND else to condition

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@ month=f.getMonth()+1;
 if( f.getDate()==24 && month==12){
     document.writeln("<h1>Happy Christmas Eve /Feliz Noche Buena</h1>");
 }
-if(f.getDate()==25 && month==12){
+else if(f.getDate()==25 && month==12){
     document.writeln("<h1>Happy Christmas/Feliz Navidad</h1>");
 }
-document.writeln("<h1>Happy Holidays</h1>");
+else {document.writeln("<h1>Happy Holidays</h1>");}
